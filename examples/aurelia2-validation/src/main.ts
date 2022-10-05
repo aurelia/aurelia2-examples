e534677c93683c4cbeb4dc2e6d15d5b1926971fd
Dwayne <dwaynecharrington@gmail.com>
2022-10-05 19:35:48 +1000

    feat(examples): use bootstrap for styling validation example

--- examples/aurelia2-validation/src/main.ts
+++ examples/aurelia2-validation/src/main.ts
@@ -3,6 +3,8 @@ import { RouterConfiguration } from '@aurelia/router';
 import { ValidationHtmlConfiguration } from '@aurelia/validation-html';
 import { MyApp } from './my-app';
 
+import 'bootstrap/dist/css/bootstrap.css';
+
 Aurelia
   .register(
     RouterConfiguration.customize({
