5d7289299394dac3f931a76df98e8947febf29ef
Dwayne <dwaynecharrington@gmail.com>
2022-10-06 10:13:37 +1000

    fix(examples): remove validation configuration

--- examples/animation-native/src/main.ts
+++ examples/animation-native/src/main.ts
@@ -1,6 +1,5 @@
 import Aurelia from 'aurelia';
 import { RouterConfiguration } from '@aurelia/router';
-import { ValidationHtmlConfiguration } from '@aurelia/validation-html';
 import { MyApp } from './my-app';
 
 import 'bootstrap/dist/css/bootstrap.css';
@@ -9,8 +8,7 @@ Aurelia
   .register(
     RouterConfiguration.customize({
       useUrlFragmentHash: false,
-    }),
-    ValidationHtmlConfiguration
+    })
   )
   .app(MyApp)
   .start();
