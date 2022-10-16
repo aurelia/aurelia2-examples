3ae21ff4cedefef2df46fc04058ce307eef914b4
dani <danicalbet@gmail.com>
2022-10-16 10:13:14 +0200

    fast framework example

---
+++ examples/fast-app/src/my-app.ts
@@ -0,0 +1,22 @@
+import { DialogDeactivationStatuses, IDialogService } from "aurelia";
+import { MyDialog } from "./my-dialog";
+
+export class MyApp {
+  public message = 'Hello World!';
+
+  constructor(@IDialogService private dialogService: IDialogService) {
+
+  }
+
+  showDialog() {
+    this.dialogService.open({
+      component: () => MyDialog,
+      lock: false
+    }).whenClosed((response) => {
+      if (response.status === DialogDeactivationStatuses.Ok) {
+        var res = <{ valueFromDialog: string }>response.value;
+        alert(res.valueFromDialog);
+      }
+    });
+  }
+}
