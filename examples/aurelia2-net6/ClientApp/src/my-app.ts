7673aceb789477b08a804f04c1ac876ade635e44
dani <danicalbet@gmail.com>
2022-10-24 18:58:16 +0200

    feat(net6): fetch data view

--- examples/aurelia2-net6/ClientApp/src/my-app.ts
+++ examples/aurelia2-net6/ClientApp/src/my-app.ts
@@ -1,3 +1,18 @@
+
 export class MyApp {
   public message = 'Hello World!';
+  static routes = [
+
+      {
+          path: [''],
+          redirectTo: 'home',
+      },
+      {
+          path: 'home',
+          component: () => import('./components/fetch-data'),
+          title: 'Home',
+          id: 'home'
+      },
+
+  ];
 }
