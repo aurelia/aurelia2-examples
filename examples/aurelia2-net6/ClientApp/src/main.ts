237fd200fbf369dabd180e7100fc1ba509b1d486
dani <danicalbet@gmail.com>
2022-10-24 18:43:35 +0200

    fetch data view

--- examples/aurelia2-net6/ClientApp/src/main.ts
+++ examples/aurelia2-net6/ClientApp/src/main.ts
@@ -1,6 +1,10 @@
-import Aurelia from 'aurelia';
+import Aurelia, { RouterConfiguration } from 'aurelia';
 import { MyApp } from './my-app';
 
 Aurelia
+.register(
+  RouterConfiguration.customize({
+    useUrlFragmentHash: false,
+  }))
   .app(MyApp)
   .start();
