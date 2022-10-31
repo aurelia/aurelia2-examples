5caa4fe2789b62b5f311e729eb7fcc050e7a1a11
dani <danicalbet@gmail.com>
2022-10-31 08:50:09 +0100

    chore(fast): fast framework examle using aurelia-fast-adapter
    It was required and update of the package to version 1.0.6 due to Aurelia breaking changes.

--- examples/fast-app/src/main.ts
+++ examples/fast-app/src/main.ts
@@ -1,4 +1,4 @@
-import Aurelia, { AppTask, DialogConfiguration, DialogService, IAttrMapper, IContainer, NodeObserverLocator } from 'aurelia';
+import Aurelia, { DialogConfiguration, DialogService } from 'aurelia';
 import { MyApp } from './my-app';
 import { FASTAdapter, FASTDialogGlobalSettings, FASTDialogRenderer } from 'aurelia-fast-adapter';
 
@@ -15,58 +15,8 @@ import {
 
 
 
-Aurelia.register(AppTask.creating(IContainer, container => {
-  const attrMapper = container.get(IAttrMapper);
-  const nodeObserverLocator = container.get(NodeObserverLocator);
-  attrMapper.useTwoWay((el, property) => {
-    switch (el.tagName) {
-      case 'FAST-SLIDER':
-      case 'FAST-TEXT-FIELD':
-      case 'FAST-TEXT-AREA':
-        return property === 'value';
-      case 'FAST-CHECKBOX':
-      case 'FAST-RADIO':
-      case 'FAST-RADIO-GROUP':
-      case 'FAST-SWITCH':
-        return property === 'checked';
-      case 'FAST-TABS':
-        return property === 'activeid';
-      default:
-        return false;
-    }
-  });
-
-  // Teach Aurelia what events to use to observe properties of elements.
-  // Because FAST components all use a single change event to notify,
-  // we can use a single common object
-  const valuePropertyConfig = { events: ['input', 'change'] };
-  nodeObserverLocator.useConfig({
-    'FAST-CHECKBOX': {
-      checked: valuePropertyConfig
-    },
-    'FAST-RADIO': {
-      checked: valuePropertyConfig
-    },
-    'FAST-RADIO-GROUP': {
-      value: valuePropertyConfig
-    },
-    'FAST-SLIDER': {
-      value: valuePropertyConfig
-    },
-    'FAST-SWITCH': {
-      checked: valuePropertyConfig
-    },
-    'FAST-TABS': {
-      activeid: valuePropertyConfig
-    },
-    'FAST-TEXT-FIELD': {
-      value: valuePropertyConfig
-    },
-    'FAST-TEXT-AREA': {
-      value: valuePropertyConfig
-    }
-  });
-}))
+Aurelia
+.register(FASTAdapter)
 .register(DialogConfiguration.customize((settings: FASTDialogGlobalSettings) => {
   settings.startingZIndex = 3;
   settings.prefix = 'ecos';
